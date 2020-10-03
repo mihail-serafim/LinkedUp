dd45cb6bc640c2110a98b116406ba254265a493a
Mihail Serafimovski <mihail.s.mk@gmail.com>
2020-10-03 16:34:51 -0400

    created server.js

---
+++ Backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+var app = express();
+
+const port = 5000;
+
+//runs python script on GET request 
+app.get('/', (req, res) => {
+ 
+	var dataToSend;
+	// spawn new child process to call the python script
+	//to pass parameters to script1.py, use [‘script1.py’,’param1’,’param2’, ...] and sys.argv[1] in python script   
+	const python = spawn('python', ['script1.py']);
+	// collect data from script
+	python.stdout.on('data', function (data) {
+		console.log('Pipe data from python script ...');
+		dataToSend = data.toString();
+	});
+	// in close event we are sure that stream from child process is closed
+	python.on('close', (code) => {
+	console.log(`child process close all stdio with code ${code}`);
+	// send data to browser
+	res.send(dataToSend)
+	});
+ 
+})
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+	// to support URL-encoded bodies
+	extended: true,
+	})
+);
+
+app.listen(port);
+ 
+logger.info("Server.js listening on port: " + port);
