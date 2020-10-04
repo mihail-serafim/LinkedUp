e0bd67d389efdfddd5f8eaf25af8ecdfcd596975
natek1234 <45215469+natek1234@users.noreply.github.com>
2020-10-03 20:27:53 -0400

    basic menu made

--- 3.14heads/src/Components/RightDashboard.js
+++ 3.14heads/src/Components/RightDashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row, Tabs, Tab, Form } from 'react-bootstrap';
+import { Col, Row, Tabs, Tab, Form, Button } from 'react-bootstrap';
 
 
 class RightDashboard extends React.Component {
@@ -7,11 +7,67 @@ class RightDashboard extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-
+            location: props.location,
+            date: props.date,
+            numberRelays: props.numberRelays,
+            frequency: props.frequency,
+            bandwidth: props.bandwidth,
+            transmitterPower: props.transmitterPower
       };
     }
+
+    //Update handlers//
+    updateLocation = (event) => {
+        console.log(event.target.value)
+        this.props.updateLocation(event.target.value)
+        this.setState({
+            location: event.target.value
+        })
+    }
+
+    updateDate = (event) => {
+        console.log(event.target.value)
+        this.props.updateLocation(event.target.value)
+        this.setState({
+            date: event.target.value
+        })
+    }
+
+    updateNumberRelays = (event) => {
+        console.log(event.target.value)
+        this.props.updateNumberRelays(event.target.value)
+        this.setState({
+            numberRelays: event.target.value
+        })
+    }
+
+    updateFrequency = (event) => {
+        console.log(event.target.value)
+        this.props.updateFrequency(event.target.value)
+        this.setState({
+            frequency: event.target.value
+        })
+    }
+
+    updateBandwidth = (event) => {
+        console.log(event.target.value)
+        this.props.updateBandwidth(event.target.value)
+        this.setState({
+            bandwidth: event.target.value
+        })
+    }
+
+    updateTransmitterPower = (event) => {
+        console.log(event.target.value)
+        this.props.updateTransmitterPower(event.target.value)
+        this.setState({
+            transmitterPower: event.target.value
+        })
+    }
+
     render() {
         return (
+            <div>
             <Tabs defaultActiveKey="Parameters" id="uncontrolled-tab-example" style={{ backgroundColor: 'white', marginTop: '10px'}}>
                 <Tab eventKey="Parameters" title='Parameters' style={{ backgroundColor: 'white', padding: '5px'}}>
                     <b>Basic</b>
@@ -19,7 +75,9 @@ class RightDashboard extends React.Component {
                         <Col>
                             <Form.Group controlId="Location">
                                 <Form.Label>Location</Form.Label>
-                                <Form.Control as="select">
+                                <Form.Control as="select"
+                                    value={this.state.location} 
+                                    onChange={this.updateLocation}>
                                     <option>Toronto, Canada</option>
                                 </Form.Control>                            
                             </Form.Group>
@@ -27,16 +85,64 @@ class RightDashboard extends React.Component {
                     </Row>
                     <Row>
                         <Col>
+                            <Form.Group controlId="Date">
+                                <Form.Label>Date</Form.Label>
+                                <Form.Control type='date'
+                                value={this.state.date} 
+                                onChange={this.updateDate} />                            
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="Relays">
+                                <Form.Label>Number of Relays</Form.Label>
+                                <Form.Control as="select" 
+                                value={this.state.numberRelays} 
+                                onChange={this.updateNumberRelays}>
+                                    <option>0</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                </Form.Control>  
+                            </Form.Group>                       
+                        </Col>
+                    </Row>
+                    <b>Wave</b>
+                    <Row>
+                        <Col>
                             <Form.Group controlId="Frequency">
                                 <Form.Label>Frequency</Form.Label>
-                                <Form.Control type="number" placeholder="5" style={{ width: '70%', display: 'inline-block', marginRight: '5px'}}/>Hz
+                                <Form.Control type="number" placeholder={0} style={{ width: '70%', display: 'inline-block', marginRight: '5px'}}
+                                        value={this.state.frequency} 
+                                        onChange={this.updateFrequency}/>Hz
                             </Form.Group>
                         </Col>
                             
                         <Col>
+                            <Form.Group controlId="Bandwidth">
+                                <Form.Label>Bandwidth</Form.Label><br></br>
+                                <Form.Control type="number" placeholder={0} style={{ width: '70%', display: 'inline-block', marginRight: '5px'}}
+                                            value={this.state.bandwidth} 
+                                            onChange={this.updateBandwidth}/>bps
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <b>Transmitter and Receiver</b>
+                    <Row>
+                        <Col>
                             <Form.Group controlId="Power">
-                                <Form.Label>Power</Form.Label><br></br>
-                                <Form.Control type="number" placeholder="5" style={{ width: '70%', display: 'inline-block', marginRight: '5px'}}/>W
+                                <Form.Label>Transmitter Power</Form.Label><br></br>
+                                <Form.Control type="number" placeholder={0} style={{ width: '70%', display: 'inline-block', marginRight: '5px'}}
+                                            value={this.state.TransmitterPower} 
+                                            onChange={this.updateTransmitterPower}/>W
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="Power">
+                                <Form.Label>Receiver Power</Form.Label><br></br>
+                                <Form.Control type="number" placeholder={0} style={{ width: '70%', display: 'inline-block', marginRight: '5px'}}
+                                            value={this.state.power} 
+                                            onChange={this.updatePower}/>W
                             </Form.Group>
                         </Col>
                     </Row>
@@ -44,10 +150,17 @@ class RightDashboard extends React.Component {
                 <Tab eventKey="Advanced Params" title="Advanced Params" style={{ backgroundColor: 'white'}}>
 
                 </Tab>
-                <Tab eventKey="Assumptions" title="Assumptions" style={{ backgroundColor: 'white'}}>
+                <Tab eventKey="Help" title="Help/How-to" style={{ backgroundColor: 'white'}}>
 
                 </Tab>
             </Tabs>
+            <Button variant="outline-primary" onClick={this.updateParameters} style={{ marginTop: '10px' }}>Update Simulation Parameters</Button>
+             <Tabs defaultActiveKey="Results" id="uncontrolled-tab-example" style={{ backgroundColor: 'white', marginTop: '10px'}}>
+                <Tab eventKey="Results" title='Results' style={{ backgroundColor: 'white', padding: '5px', marginBottom: '10px'}}>
+                    hi
+                </Tab>
+            </Tabs>
+            </div>
         );
     }
 }
--- 3.14heads/src/Dashboard.js
+++ 3.14heads/src/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Button } from 'react-bootstrap';
 import PageHeader from './Components/PageHeader';
 import PageFooter from './Components/PageFooter';
 import RightDashboard from './Components/RightDashboard';
@@ -9,9 +9,64 @@ class Dashboard extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
+          location: 'Toronto, Canada',
+          date: null,
+          numberRelays: 0,
+          frequency: null,
+          bandwidth: null,
+          transmitterPower: null,
+
 
       };
     }
+    
+    //Update handlers//
+    updateNumberRelays = (numberRelays) => {
+        console.log(numberRelays)
+        this.setState({
+            numberRelays: numberRelays
+        })
+    }
+
+    updateDate = (date) => {
+        console.log(date)
+        this.setState({
+            date: date
+        })
+    }
+
+    updateFrequency = (frequency) => {
+        console.log(frequency)
+        this.setState({
+            frequency: frequency
+        })
+    }
+
+    updateBandwidth = (bandwidth) => {
+        console.log(bandwidth)
+        this.setState({
+            bandwidth: bandwidth
+        })
+    }
+
+    updateTransmitterPower = (power) => {
+        console.log(power)
+        this.setState({
+            transmitterPower: power
+        })
+    }
+
+    updateLocation = (location) => {
+        console.log(location)
+        this.setState({
+            location: location
+        })
+    }
+
+    updateParameters = async() => {
+        let results = await updateParametersEndpoint();
+    }
+
 
     render() {
 
@@ -25,7 +80,20 @@ class Dashboard extends React.Component {
 
                         </Col>
                         <Col sm={4} style={{ border: 'solid', height: '100%'}}>
-                            <RightDashboard />
+                            <RightDashboard 
+                            location={this.state.location}
+                            date={this.state.date}
+                            numberRelays={this.state.numberRelays}
+                            frequency={this.state.frequency}
+                            bandwidth={this.state.bandwidth}
+                            transmitterPower={this.state.transmitterPower} 
+                            updateLocation={this.updateLocation}
+                            updateDate={this.updateDate}   
+                            updateNumberRelays={this.updateNumberRelays}
+                            updateFrequency={this.updateFrequency}
+                            updateBandwidth={this.updateBandwidth}
+                            updateTransmitterPower={this.updateTransmitterPower}                        
+                            />
                         </Col>
                     </Row>
 
@@ -36,5 +104,20 @@ class Dashboard extends React.Component {
         );
     }
 }
+
+//Endpoints
+async function updateParametersEndpoint() {
+    let userJSON = {};
+    userJSON.projectID = null;
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userJSON),
+    };
+   
+    const response = await fetch("/updateParameters", requestOptions);
+    let data = await response.json();
+    return data;
+  }
 
 export default Dashboard;
