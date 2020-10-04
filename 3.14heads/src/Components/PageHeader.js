26a3560a2b56b9dc9d61fef6586838d22522485b
natek1234 <45215469+natek1234@users.noreply.github.com>
2020-10-04 16:15:37 -0400

    added team logo

--- 3.14heads/src/Components/PageHeader.js
+++ 3.14heads/src/Components/PageHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import logo from '../assets/Square_White.svg';
+import teamLogo from '../assets/White_chain_logo.png';
 
 class PageHeader extends React.Component {
 
@@ -14,6 +15,7 @@ class PageHeader extends React.Component {
         return (
             <Navbar style={{ paddingTop: '15px', paddingBottom: '15px'}} bg="dark" expand="lg" variant='dark' id='NavBar'>
                 <Navbar.Brand ><b style={{ fontSize: '25px', paddingLeft: '15px'}}>LinkedUp</b></Navbar.Brand>
+                <img style={{ width: '4%', marginLeft: '-5px'}} src={teamLogo}/>
                 <div style={{width: '100%'}}><img src={logo} style={{ float: 'right', width: '70px', paddingRight: '10px', marginTop: '0px', marginBottom: '0px'}}/></div>
             </Navbar>
         );
